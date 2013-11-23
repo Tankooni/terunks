189c98d8a948ebcad90b4aeb2935f34d3ecd7b2b
Zack Wheeler <913966+Tankooni@users.noreply.github.com>
2013-11-23 13:36:33 -0500

    Needing to commit so i can merge with the level stuff for testing.

--- Room.cs
+++ Room.cs
@@ -1,5 +1,7 @@
 ﻿using Punk;
 using System;
+using System.Collections.Generic;
+using GameObjects;
 
 namespace NHTI
 {
@@ -8,6 +10,7 @@ namespace NHTI
 	/// </summary>
 	public class Room : Entity
 	{
+		public List<Door> Doors = new List<Door>();
 		public Room()
 		{
 		}
