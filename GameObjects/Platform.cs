4f445cbf87b33b57c74f62ca41070564b3efd56a
Tankooni <Tankooni@localhost>
2013-11-23 00:43:21 -0500

    Ogmo project started.

---
+++ GameObjects/Platform.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Croanc
+ * Date: 11/23/2013
+ * Time: 12:37 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace NHTI.GameObjects
+{
+	/// <summary>
+	/// Description of Platform.
+	/// </summary>
+	public class Platform
+	{
+		public Platform()
+		{
+		}
+	}
+}
