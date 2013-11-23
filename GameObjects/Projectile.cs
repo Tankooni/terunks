c1ef8eef74c7d73e8a7c1e674a356d0e9792e0ec
Godot <Godot@localhost>
2013-11-23 17:53:41 -0500

    Added assets for the snowhead and snowball, basic projectile and snowhead hat class

---
+++ GameObjects/Projectile.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Matte
+ * Date: 11/23/2013
+ * Time: 5:05 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Punk;
+using Punk.Graphics;
+
+namespace NHTI.GameObjects
+{
+	/// <summary>
+	/// Description of Projectile.
+	/// </summary>
+	public class Projectile
+	{
+		public Nineslice image;
+		
+		int xVelocity = 0;
+		int yVelocity = 0;
+		
+		public Projectile(int sentXVel, int sentYVel)
+		{
+			image = new Nineslice(Library.GetTexture("assets/Snowball.png"), 3, 3);
+			xVelocity = sentXVel;
+			yVelocity = sentYVel;
+		}
+		
+		public void Update()
+		{
+			image.X += xVelocity;
+			image.Y += yVelocity;
+		}
+	}
+}
---
+++ Hats/SnowHat.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Matte
+ * Date: 11/23/2013
+ * Time: 5:18 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace NHTI.Hats
+{
+	/// <summary>
+	/// Description of SnowHat.
+	/// </summary>
+	public class SnowHat {
+		
+		public SnowHat()
+		{
+			
+		}
+	}
+}
