023e0c806fa94ea231435e60ee087471abc04263
Godot <Godot@localhost>
2013-11-24 02:03:49 -0500

    Created the COMPLETE ENEMY CLASS without ANY ASSISTANCE
    Beacuaz I am gud progamer

--- GameObjects/Enemy.cs
+++ GameObjects/Enemy.cs
@@ -9,7 +9,10 @@
 using System;
 using Punk;
 using Punk.Graphics;
-
+using Punk.Tweens.Misc;
+using Punk.Utils;
+using SFML.Window;
+using System.Collections.Generic;
 
 namespace GameObjects
 {
@@ -19,15 +22,40 @@ namespace GameObjects
 	/// 
 	public class Enemy : Entity
 	{
-		public Enemy()
-		{
-			
-		}
+		List<Vector2i> positionNodes = new List<Vector2i>();
+		
+		Vector2i currentPosition = new Vector2i(0,0);
+		
+		int nodeIndex = 0;
+		
+		public Enemy() {}
 		
 		public override void Load(System.Xml.XmlNode node)
 		{
 			base.Load(node);
-			Graphic = Image.CreateRect(50, 50, FP.Color(0x66FF33));
+			Graphic = Image.CreateRect(64, 64, FP.Color(0x66FF33));
+			
+			foreach (System.Xml.XmlNode n in node)
+			{
+				FP.Log(int.Parse(n.Attributes["x"].Value) + " " + float.Parse(n.Attributes["y"].Value));
+				positionNodes.Add(new Vector2i(int.Parse(n.Attributes["x"].Value), int.Parse(n.Attributes["y"].Value)));
+			}
+			currentPosition = positionNodes[0];
+			MoveToNextPos();
+		}
+		
+		public void MoveToNextPos()
+		{
+			currentPosition = FP.Next(currentPosition, positionNodes, true);
+			var twoon = new MultiVarTween(MoveToNextPos, ONESHOT);
+			twoon.Tween(this, new { X = currentPosition.X, Y = currentPosition.Y}, 5.0f, Ease.ElasticIn);
+			AddTween(twoon, true);
+		}
+		
+		public override void Update()
+		{
+			base.Update();
+			
 		}
 	}
 }
