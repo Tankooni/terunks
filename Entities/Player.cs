73a1e70d6a99dff3071590e8295a9d47777509b6
Tankooni <Tankooni@localhost>
2013-11-23 21:21:09 -0500

    Swapping levels through doors.

--- Entities/Player.cs
+++ Entities/Player.cs
@@ -25,6 +25,7 @@ namespace NHTI.Entities
 	/// </summary>
 	public class Player : Entity
 	{
+		public const string Collision = "Player";
 		public uint id;
 		public Controller controller;
 		
@@ -96,6 +97,7 @@ namespace NHTI.Entities
 		
 		public Player(float x, float y, uint id) : base(x,y)
 		{
+			Type = Collision;
 			this.id = id;
 			this.controller = new Controller(id);
 			
@@ -235,10 +237,7 @@ namespace NHTI.Entities
 		public override void Added()
 		{
 			base.Added();
-			List<Entity> l = new List<Entity>();
-			World.GetType("PlayerSpawn",l);
-			X = l[0].X;
-			Y = l[0].Y;
+			
 		}
 		
 	}
