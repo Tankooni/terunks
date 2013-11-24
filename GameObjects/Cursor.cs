22fdae5a09ccaf59ffd1a116e6be75691a7d0527
Tankooni <Tankooni@localhost>
2013-11-24 02:06:25 -0500

    Cursor.

---
+++ GameObjects/Cursor.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using NHTI;
+using Punk;
+using Punk.Graphics;
+using NHTI.Entities;
+using GameObjects;
+
+namespace GameObjects
+{
+	/// <summary>
+	/// Description of Cursor.
+	/// </summary>
+	public class Cursor : Entity
+	{
+		public Cursor()
+		{
+			Graphic = Image.CreateCircle(10, FP.Color(0x553366));
+			(Graphic as Image).CenterOrigin();
+			FP.Screen.SetMouseCursorVisible(false);
+			Layer = -10000;
+		}
+		public override void Update()
+		{
+			base.Update();
+			X = World.MouseX;
+			Y = World.MouseY;
+		}
+	}
+}
