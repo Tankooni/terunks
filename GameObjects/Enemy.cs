a90480e934fe82f2bf87f485edc17316a5845db6
Godot <Godot@localhost>
2013-11-24 00:36:00 -0500

    DID SOME STUFF YOU GUYS I'M A GUD PROGRAMER

---
+++ GameObjects/Enemy.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Matte
+ * Date: 11/23/2013
+ * Time: 11:35 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Punk;
+using Punk.Graphics;
+
+
+namespace NHTI.GameObjects
+{
+	/// <summary>
+	/// Description of Enemy.
+	/// </summary>
+	/// 
+	public class Enemy : Entity
+	{
+		
+		Graphic = Image.CreateRect(50, 50, FP.Color(0x66FF33);
+		
+		public override void Load(System.Xml.XmlNode node)
+		{
+			base.Load(node);
+		}
+		
+		public Enemy()
+		{
+			
+		}
+	}
+}
